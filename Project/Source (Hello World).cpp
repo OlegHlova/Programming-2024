ec6cfae45d886e2d0b20082a52483022e4e19830
Oleg Hlova (st) <114058765+OlegHlova@users.noreply.github.com>
2022-09-25 20:03:14 +0300

    Programing
    Programing

---
+++ Project/Source (Hello World).cpp
@@ -0,0 +1,26 @@
+/*/#include <iostream>
+#include  <string>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	/*cout << "Hello world" << endl;
+
+	cout<<"7 + 3"<<7+3<<endl;
+	cout << 876.12 << endl;
+	cout << "f" << endl;
+	int number;
+	cout << "Enter your fauvorite number" << endl;
+	cin >> numberж;*/
+
+
+	*//return 0;*/
+		//ввести сторону квадрата
+float side;
+cout << "Enter the side" << endl;
+cin >> side; 
+s= side*side 
+cout << "S=" << s << endl;
+cout << "P=" << 4 * side << endl;
+return 0;
