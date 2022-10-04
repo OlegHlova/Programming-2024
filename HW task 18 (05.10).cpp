c2822f007dc4a92eab34f022a28dc0c94835d3ad
OlehHlova <hlovaoleg2747@gmail.com>
2022-10-04 18:45:25 +0300

    project

---
+++ HW task 18 (05.10).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+    for (int i = 0; i < 30; i++)
+    {
+        for (int j = 0; j < 30; j++)
+        {
+            for (int k = 0; k < 30; k++)
+            {
+                cout << i << j << k << endl;
+            }
